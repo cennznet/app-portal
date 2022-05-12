7e3b74174c1265d8e6206fcf1e28828730c6a543
aidan-starke <starke.aidan@gmail.com>
2022-05-13 09:55:23 +1200

    prettier format

--- src/libs/Routes/assets.ts
+++ src/libs/Routes/assets.ts
@@ -1,23 +1,20 @@
 // Copyright 2017-2022 @polkadot/apps-routing authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { TFunction } from 'i18next';
-import type { Route } from './types';
+import type { TFunction } from "i18next";
+import type { Route } from "./types";
 
-import Component from '@polkadot/app-assets';
+import Component from "@polkadot/app-assets";
 
-export default function create (t: TFunction): Route {
-  return {
-    Component,
-    display: {
-      needsApi: [
-        'tx.assets.setMetadata',
-        'tx.assets.transferKeepAlive'
-      ]
-    },
-    group: 'network',
-    icon: 'shopping-basket',
-    name: 'assets',
-    text: t('nav.assets', 'Assets', { ns: 'apps-routing' })
-  };
+export default function create(t: TFunction): Route {
+	return {
+		Component,
+		display: {
+			needsApi: ["tx.assets.setMetadata", "tx.assets.transferKeepAlive"],
+		},
+		group: "network",
+		icon: "shopping-basket",
+		name: "assets",
+		text: t("nav.assets", "Assets", { ns: "apps-routing" }),
+	};
 }
--- src/libs/Routes/index.ts
+++ src/libs/Routes/index.ts
@@ -1,31 +1,31 @@
 // Copyright 2017-2022 @polkadot/apps-routing authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { TFunction } from 'i18next';
-import type { Routes } from '@/libs/Routes/types';
+import type { TFunction } from "i18next";
+import type { Routes } from "@/libs/Routes/types";
 
-import accounts from '@/libs/Routes/accounts';
-import addresses from '@/libs/Routes/addresses';
-import explorer from '@/libs/Routes/explorer';
-import extrinsics from '@/libs/Routes/extrinsics';
-import files from '@/libs/Routes/files';
-import js from '@/libs/Routes/js';
-import rpc from '@/libs/Routes/rpc';
-import settings from '@/libs/Routes/settings';
-import signing from '@/libs/Routes/signing';
-import sudo from '@/libs/Routes/sudo';
+import accounts from "@/libs/Routes/accounts";
+import addresses from "@/libs/Routes/addresses";
+import explorer from "@/libs/Routes/explorer";
+import extrinsics from "@/libs/Routes/extrinsics";
+import files from "@/libs/Routes/files";
+import js from "@/libs/Routes/js";
+import rpc from "@/libs/Routes/rpc";
+import settings from "@/libs/Routes/settings";
+import signing from "@/libs/Routes/signing";
+import sudo from "@/libs/Routes/sudo";
 
-export default function create (t: TFunction): Routes {
-  return [
-    accounts(t),
-    addresses(t),
-    explorer(t),
-    extrinsics(t),
-    rpc(t),
-    signing(t),
-    sudo(t),
-    files(t),
-    js(t),
-    settings(t)
-  ];
+export default function create(t: TFunction): Routes {
+	return [
+		accounts(t),
+		addresses(t),
+		explorer(t),
+		extrinsics(t),
+		rpc(t),
+		signing(t),
+		sudo(t),
+		files(t),
+		js(t),
+		settings(t),
+	];
 }
