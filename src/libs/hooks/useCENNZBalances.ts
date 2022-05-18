efb949e0c929df44e4cee7c4a556a947416b20bf
aidan-starke <starke.aidan@gmail.com>
2022-05-18 14:56:27 +1200

    fetch locked balance

--- src/libs/hooks/useCENNZBalances.ts
+++ src/libs/hooks/useCENNZBalances.ts
@@ -1,19 +1,32 @@
-import type { Balance } from "@cennznet/types";
+import type { Balance, Codec } from "@cennznet/types";
 
-import { createNamedHook, useApi, useCallMulti } from "@polkadot/react-hooks";
+import {
+	createNamedHook,
+	useApi,
+	useCall,
+	useCallMulti,
+} from "@polkadot/react-hooks";
 
 /**
  * Gets CENNZ balances for account
  *
  * @param accountAddress The account address of which balance is to be returned
+ * @param type The type of balance to return
  * @returns CENNZ balances
  */
-function useCENNZBalancesImpl(accountAddress: string) {
+function useCENNZBalancesImpl(
+	accountAddress: string,
+	type: "free" | "locked"
+	// @ts-ignore
+): Pick<Balance[], Codec> {
 	const { api, systemChain } = useApi();
 
 	const isAzalea = systemChain.includes("Azalea");
 	const CENNZ = isAzalea ? "1" : "16000";
 	const CPAY = isAzalea ? "2" : "16001";
+
+	if (type === "locked")
+		return useCall<any>(api.query.genericAsset.locks, [CENNZ, accountAddress]);
 
 	return useCallMulti<Balance[]>(
 		[CENNZ, CPAY].map((assetId) => [
